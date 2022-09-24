14a7a55fb04ec42334016da8710b925cc49e5816
Andres Godoy <andresegm@gmail.com>
2022-09-24 17:15:40 -0600

    Write function to add every number in range of arguments passed to function (inclusive). Test function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
-};
+const sumAll = (int1, int2) => {
+    if (typeof(int1) !== 'number' || typeof(int2) !== 'number')  {
+      return 'ERROR';
+    } else if (int1 < 0 || int2 < 0) {
+        return 'ERROR';
+    }
+    let arr = [];
+    let sum = 0;
+    if (int2 > int1) {
+      arr = [int1, int2] ;
+    } else {
+      arr = [int2, int1];
+    }
+    for (let i = arr[0]; i <= arr[1]; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  
 
 // Do not edit below this line
 module.exports = sumAll;
