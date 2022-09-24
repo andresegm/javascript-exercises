a6a5a761ca5bdf1702c6250ca6565d93ed812eb6
Andres Godoy <andresegm@gmail.com>
2022-09-24 16:46:55 -0600

    Write function to remove matching elements from array using a nested loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = (arr, ...els) => {
+  for (let i = 0; i < els.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (els[i] === arr[j]) {
+        arr.push(arr.splice(arr.indexOf(arr[j]), 1)[0]); //pushes the matched element to the end of the arr array
+        arr.pop(); //removes the last element of the array
+      }
+    }
+  }
+  return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
