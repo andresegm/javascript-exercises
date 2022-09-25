c29829d9216808db52222776d426088b1af92d74
Andres Godoy <andresegm@gmail.com>
2022-09-24 18:29:32 -0600

    write functions to convert from F to C and vice-versa. Test functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let celsius = Math.round(((tempF - 32) * 5/9)*10)/10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let fahrenheit = Math.round(((tempC * 9/5) + 32)*10)/10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
