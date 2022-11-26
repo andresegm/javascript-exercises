9655502ca0db48dd3eae61a5f2e3180b91994834
Andres Godoy <andresegm@gmail.com>
2022-11-25 18:19:19 -0700

    refractored if/else code into ternary operator

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -3,11 +3,7 @@ const findTheOldest = function(arr) {
         return (obj.yearOfDeath ? (obj.yearOfDeath - obj.yearOfBirth) : (2022 - obj.yearOfBirth))
     }
     const arrSorted = arr.sort((a, b) => {
-        if (calculateAge(a) > calculateAge(b)) {
-            return -1
-        } else {
-            return 1
-        }
+        return ((calculateAge(a) > calculateAge(b)) ? -1 : 1 )
     })
     return arrSorted[0]
 };
