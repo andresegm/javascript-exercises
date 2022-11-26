0048683e37b672a4ccb0fb04a6c718cef793f645
Andres Godoy <andresegm@gmail.com>
2022-11-25 17:19:31 -0700

    write code for Fibonacci function; tested function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    } 
+    const numArr = [0, 1]   
+    for (let i = 1; i < num; i++) {
+        numArr.push((numArr[i] + numArr[i-1]))
+    }
+    return numArr[num]
 };
 
+console.log(fibonacci(20))
 // Do not edit below this line
 module.exports = fibonacci;
