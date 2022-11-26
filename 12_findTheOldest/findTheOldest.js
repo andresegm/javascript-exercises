2805513de892dfd21541688d7512db6a2e0360f8
Andres Godoy <andresegm@gmail.com>
2022-11-25 18:15:45 -0700

    write code to calculate age and sort array based on age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const calculateAge = (obj) => {
+        return (obj.yearOfDeath ? (obj.yearOfDeath - obj.yearOfBirth) : (2022 - obj.yearOfBirth))
+    }
+    const arrSorted = arr.sort((a, b) => {
+        if (calculateAge(a) > calculateAge(b)) {
+            return -1
+        } else {
+            return 1
+        }
+    })
+    return arrSorted[0]
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
